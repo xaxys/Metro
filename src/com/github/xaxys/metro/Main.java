6bc50e08c2e913f3432eab509f7b2f3d551cb0b6
Chen Wu <201436728@qq.com>
2023-01-03 00:26:56 +0800

    Add TabExecutor
    Fix a bug in loop command
    MetroLine can't contain space now
    Update to 1.4.4

--- src/com/github/xaxys/metro/Main.java
+++ src/com/github/xaxys/metro/Main.java
@@ -1,37 +1,34 @@
 package com.github.xaxys.metro;
 
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements CommandExecutor {
-	
-	public static Main plugin;
+
 	public static final String PERM_RELOAD = "metro.reload";
 	public static final String PERM_CREATE = "metro.create";
 	public static final String PERM_SPEED = "metro.speed";
 	public static final String PERM_USE = "metro.use";
-	
+	public static Main plugin;
+
 	@Override
 	public void onEnable() {
 		plugin = this;
 		Conf.initDefaults();
 
 		Conf.loadConfig();
-		Bukkit.getConsoleSender().sendMessage(Conf.MSG_DBG + "§bConfiguration Loaded!");
+		getServer().getConsoleSender().sendMessage(Conf.MSG_DBG + "§bConfiguration Loaded!");
 
 		DataBase.DB = DataBase.readDB(Conf.DB_PATH);
-		Bukkit.getConsoleSender().sendMessage(Conf.MSG_DBG + String.format("§b%d Lines and %d Stations Loaded!",
+		getServer().getConsoleSender().sendMessage(Conf.MSG_DBG + String.format("§b%d Lines and %d Stations Loaded!",
 				DataBase.DB.MetroMap.size(), DataBase.DB.PositionMap.size()));
 
+		getCommand("metro").setExecutor(new CommandHandler());
 		getServer().getPluginManager().registerEvents(new EventListener(), this);
-		Bukkit.getConsoleSender().sendMessage(Conf.MSG_DBG + "§bEventListener Registered!");
+		getServer().getConsoleSender().sendMessage(Conf.MSG_DBG + "§bCommandHandler and EventListener Registered!");
 
-		Bukkit.getConsoleSender().sendMessage(Conf.MSG_DBG + "§bMetro Plugin Loaded!");
+		getServer().getConsoleSender().sendMessage(Conf.MSG_DBG + "§bMetro Plugin Loaded!");
 	}
 	
 	@Override
@@ -39,115 +36,6 @@ public class Main extends JavaPlugin implements CommandExecutor {
 		HandlerList.unregisterAll();
 		DataBase.DB.saveDB(Conf.DB_PATH);
 		saveConfig();
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(command.getName().equalsIgnoreCase("metro")) {
-			SuccessfulHandle:
-			{
-				if (args.length == 0) break SuccessfulHandle;
-				if (args[0].equalsIgnoreCase("loop")) {
-
-					if (!(sender instanceof ConsoleCommandSender) && !sender.hasPermission(PERM_CREATE)) {
-						sender.sendMessage(Conf.MSG_NOPERM);
-						break SuccessfulHandle;
-					}
-
-					if (args.length == 3) {
-
-						// switch a line to loop line/straight line
-
-						boolean isLoop = false;
-						if (args[2].equalsIgnoreCase("t") || args[1].equalsIgnoreCase("true")) {
-							isLoop = true;
-						}
-						if (DataBase.DB.setLoop(args[1], isLoop)) {
-							sender.sendMessage(Conf.MSG_DBG + "MetroLine " + args[1] + " isLoop set to " + isLoop);
-						} else {
-							sender.sendMessage(Conf.MSG_DBG + "MetroLine " + args[1] + " not found");
-						}
-					} else if (args.length == 2) {
-
-						// get loop status of line
-
-						Boolean isLoop = DataBase.DB.getLoop(args[1]);
-						if (isLoop != null) {
-							sender.sendMessage(Conf.MSG_DBG + "MetroLine " + args[1] + " isLoop is " + isLoop);
-						} else {
-							sender.sendMessage(Conf.MSG_DBG + "MetroLine " + args[1] + " not found");
-						}
-					} else {
-						break SuccessfulHandle;
-					}
-				} else if (args[0].equalsIgnoreCase("speed")) {
-
-					if (!(sender instanceof ConsoleCommandSender) && !sender.hasPermission(PERM_SPEED)) {
-						sender.sendMessage(Conf.MSG_NOPERM);
-						break SuccessfulHandle;
-					}
-
-					if (args.length == 3) {
-
-						// set speed of line
-
-						double speed = Conf.NORMAL_SPEED;
-						try {
-							speed = Double.parseDouble(args[2]);
-						} catch (NumberFormatException e) { }
-
-						if (DataBase.DB.setSpeed(args[1], speed)) {
-							sender.sendMessage(Conf.MSG_DBG + "MetroLine " + args[1] + " speed set to " + speed);
-						} else {
-							sender.sendMessage(Conf.MSG_DBG + "MetroLine " + args[1] + " not found");
-						}
-					} else if (args.length == 2) {
-
-						// display speed of line
-
-						Double speed = DataBase.DB.getSpeed(args[1]);
-						if (speed != null) {
-							sender.sendMessage(Conf.MSG_DBG + "MetroLine " + args[1] + " speed is " + speed);
-						} else {
-							sender.sendMessage(Conf.MSG_DBG + "MetroLine " + args[1] + " not found");
-						}
-					} else {
-						break SuccessfulHandle;
-					}
-				} else if (args[0].equalsIgnoreCase("reload")) {
-
-					if (!(sender instanceof ConsoleCommandSender) && !sender.hasPermission(PERM_RELOAD)) {
-						sender.sendMessage(Conf.MSG_NOPERM);
-						break SuccessfulHandle;
-					}
-
-					// reload configuration
-
-					Conf.loadConfig();
-					sender.sendMessage("§aMetro Plugin Reloaded!");
-				} else if (args[0].equalsIgnoreCase("debug")) {
-
-					if (!(sender instanceof ConsoleCommandSender) && !sender.hasPermission(PERM_RELOAD)) {
-						sender.sendMessage(Conf.MSG_NOPERM);
-						break SuccessfulHandle;
-					}
-
-					// enable/disable debug mode
-
-					Conf.DEBUG = !Conf.DEBUG;
-					getConfig().set("debug", Conf.DEBUG);
-					sender.sendMessage("§aMetro Plugin DebugMode:" + Conf.DEBUG);
-				}
-				return true;
-			}
-
-			UnsuccessfulHandle:
-			{
-				sender.sendMessage(Conf.MSG_USAGE);
-				return true;
-			}
-		}
-		return false;
 	}
 
 }
--- src/com/github/xaxys/metro/Util.java
+++ src/com/github/xaxys/metro/Util.java
@@ -8,14 +8,14 @@ public final class Util {
 	
 	public static void setPowered(Block b, boolean on) {
 		if (b.getBlockData() instanceof Switch) {
-			Switch s = (Switch)b.getBlockData();
+			Switch s = (Switch) b.getBlockData();
 			s.setPowered(on);
 			b.setBlockData(s);
 		}
 	}
 
 	public static void setSign(Block sign, String[] lines) {
-		Sign s = ((Sign)sign.getState());
+		Sign s = ((Sign) sign.getState());
 		s.setLine(0, lines[0] == null ? Conf.TITLE : lines[0]);
 		s.setLine(1, lines[1] == null ? "" : lines[1]);
 		s.setLine(2, lines[2] == null ? "" : lines[2]);
@@ -24,7 +24,7 @@ public final class Util {
 	}
 
 	public static void setSign(Block sign, String title) {
-		Sign s = ((Sign)sign.getState());
+		Sign s = ((Sign) sign.getState());
 		s.setLine(0, title == null ? Conf.TITLE : title);
 		s.setLine(1, "");
 		s.setLine(2, "");
@@ -33,12 +33,12 @@ public final class Util {
 	}
 
 	public static void setLine(Block sign, int l, String str) {
-		Sign s = ((Sign)sign.getState());
+		Sign s = ((Sign) sign.getState());
 		s.setLine(l, str == null ? "" : str);
 		s.update();
 	}
 
 	public static String[] getLines(Block sign) {
-		return ((Sign)sign.getState()).getLines();
+		return ((Sign) sign.getState()).getLines();
 	}
 }
